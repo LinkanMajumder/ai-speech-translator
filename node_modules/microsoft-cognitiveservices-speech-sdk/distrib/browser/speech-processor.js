e3c937cf9957c488a3616a18ef19f0ef6271de39
Linkan <linkan469@gmail.com>
2021-12-27 16:29:01 +0530

    initial commit

--- node_modules/microsoft-cognitiveservices-speech-sdk/distrib/browser/speech-processor.js
+++ node_modules/microsoft-cognitiveservices-speech-sdk/distrib/browser/speech-processor.js
@@ -10,7 +10,7 @@ class SpeechProcessor extends AudioWorkletProcessor {
 
   process(inputs, outputs) {
     const input = inputs[0];
-    const output = []; // Make sure output array can be assigned to
+    const output = outputs[0];
     for (let channel = 0; channel < input.length; channel += 1) {
       output[channel] = input[channel];
     }
